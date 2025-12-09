9a7d2d84a53818723c9a2d3686262a96cfde86a8
TQHYG <tqhyg@outlook.com>
2025-12-09 23:25:47 +0800

    modified:   android/src/main/java/com/tailscale/ipn/ui/Links.kt 	modified:   android/src/main/res/values/strings.xml

--- android/src/main/java/com/tailscale/ipn/ui/Links.kt
+++ android/src/main/java/com/tailscale/ipn/ui/Links.kt
@@ -4,10 +4,10 @@
 package com.tailscale.ipn.ui
 
 object Links {
-  const val DEFAULT_CONTROL_URL = "https://headplane.tqhyg.net"
-  const val SERVER_URL = "https://headscale.tqhyg.net"
-  const val ADMIN_URL = SERVER_URL + "/ouroboros"
-  const val SIGNIN_URL = "https://headscale.tqhyg.net/register"
+  const val DEFAULT_CONTROL_URL = "https://controlplane.tailscale.com"
+  const val SERVER_URL = "https://login.tailscale.com"
+  const val ADMIN_URL = SERVER_URL + "/admin"
+  const val SIGNIN_URL = "https://tailscale.com/login"
   const val PRIVACY_POLICY_URL = "https://tailscale.com/privacy-policy/"
   const val TERMS_URL = "https://tailscale.com/terms"
   const val DOCS_URL = "https://tailscale.com/kb/"
